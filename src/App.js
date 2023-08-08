92898eac08064347bb225de1f743deae2c8c6394
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-08-09 00:22:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMoviesHandler = async () => {
-    const response = await fetch("https://swapi.dev/api/films/");
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
 
-    const transformedData = data.results.map((movieData) => {
+    const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
         title: movieData.title,
         openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+        releaseDate: movieData.release_date,
       };
     });
-    setMovies(transformedData);
-  };
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
 
   return (
     <React.Fragment>
@@ -27,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
